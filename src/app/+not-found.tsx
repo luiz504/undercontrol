3a79791d2dbb16981cf03be735d3f671dff935f9
luiz bueno <luiz504@hotmail.com>
2024-10-13 20:16:23 -0300

    style: update not-found-page

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,40 +1,25 @@
 import { Link, Stack } from 'expo-router'
-import { StyleSheet } from 'react-native'
 
-import { Text, View } from '~/components/Themed'
+import {
+  GluestackUIProvider,
+  Heading,
+  SafeAreaView,
+  Text,
+} from '~/components/ui'
 
 export default function NotFoundScreen() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn&apos;t exist.</Text>
+    <GluestackUIProvider>
+      <Stack.Screen options={{ title: 'Oops!', headerShown: false }} />
+      <SafeAreaView className="flex-1 items-center justify-center bg-slate-950 p-8">
+        <Heading className="mb-12">This screen doesn&apos;t exist.</Heading>
 
-        <Link href="/" style={styles.link}>
-          <Text style={styles.linkText}>Go to home screen!</Text>
+        <Link href="/">
+          <Text className="text-lg font-semibold uppercase text-purple-500">
+            Go to home screen!
+          </Text>
         </Link>
-      </View>
-    </>
+      </SafeAreaView>
+    </GluestackUIProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-})
