fd1c25ea7222cc2c6e531643bd586ae49e339c09
luiz bueno <luiz504@hotmail.com>
2024-11-20 12:23:39 -0300

    chore: install/implement react-query debugger

--- src/infra/cache/query-provider.tsx
+++ src/infra/cache/query-provider.tsx
@@ -1,9 +1,14 @@
-import { QueryClientProvider } from '@tanstack/react-query'
 import { FC, PropsWithChildren } from 'react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { DevToolsBubble } from 'react-native-react-query-devtools'
+
 import { queryClient } from './query-client'
 
 export const QueryProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <DevToolsBubble />
+    </QueryClientProvider>
   )
 }
